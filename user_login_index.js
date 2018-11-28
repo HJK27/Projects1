03c62e1f62a647e47b70105e2bc4a20c97351971
Kevin Pai <KPai555@Hotmail.com>
2018-11-28 13:04:41 -0800

    Added login and registration functions

--- user_login_index.js
+++ user_login_index.js
@@ -1,13 +1,15 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-	document.getElement.ById("userLogin").style.display = "none";
-	document.getElement.ById("welcome").style.display = "initial";
-	window.alert("Login successful!");
 	
-    //window.location = './G2L_Map.html';
+	document.getElementById("submission_form").style.display = "none";
+	document.getElementById("userLogin").style.display = "none";
+	document.getElementById("register").style.display = "none";
+	document.getElementById("welcome").style.display = "initial";
   } else {
-	  document.getElement.ById("welcome").style.display = "none";
-	  document.getElement.ById("userLogin").style.display = "initial";
+	  
+	document.getElementById("submission_form").style.display = "initial";
+	document.getElementById("userLogin").style.display = "block";
+	document.getElementById("welcome").style.display = "none";
   }
 });
 
@@ -16,8 +18,7 @@ function login() {
 	var userEmail = document.getElementById("email_field").value;
 	var userPass = document.getElementById("password_field").value;
 	
-	window.alert("Login successful!");
-	//window.alert(userEmail + " " + userPass);
+	document.getElementById("welcome").innerHTML = "Welcome " + userEmail + "!";
 
 firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
   // Handle Errors here.
@@ -28,5 +29,24 @@ firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(e
 }
 
 function logout() {
-	firebase.auth().signOut();
+firebase.auth().signOut().then(function() {
+  //window.alert("Logout successful!");
+}).catch(function(error) {
+  // An error happened.
+});
+}
+
+function newUser() {
+	
+	var newEmail = document.getElementById("new_email_field").value;
+	var newPass = document.getElementById("new_password_field").value;
+	
+	document.getElementById("welcome").innerHTML = "Welcome " + newEmail + "!";
+	
+firebase.auth().createUserWithEmailAndPassword(newEmail, newPass).catch(function(error) {
+  //Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  window.alert("Error: "  + errorMessage);
+});
 }
